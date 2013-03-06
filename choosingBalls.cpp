5ac997224e52733abe8cfc3b8bb267286eabf7b0
Himanshu Singh <91.himanshu@gmail.com>
2013-03-06 22:48:37 +0530

    nice dp

--- choosingBalls.cpp
+++ choosingBalls.cpp
@@ -1,18 +1,54 @@
-#include<iostream>
-#include<cstring>
+/* Problem Link: http://www.codeforces.com/contest/264/problem/C */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
 using namespace std;
-const int MAX_N=1e5+7;
-typedef long long ll;
-ll v[MAX_N], c[MAX_N], mc[MAX_N];
-ll dp[MAX_N][2];
+const int MAX_N = int(1e5)+7;
+
+long long dp[MAX_N];
+bool valid[MAX_N];
+long long val[MAX_N];
+int color[MAX_N];
+
 int main()
 {
-  int n, queries;
-  cin>>n>>queries;
-  for (int i = 1; i <= n; i++) cin>>v[i];
-  for (int i = 1; i <= n; i++) cin>>c[i];
-  for (int q = 1; q <= queries; q++) {
+	int n, queries;
+	cin>>n>>queries;
+
+	for (int i = 1; i <= n; i++) cin>>val[i];
+	for (int i = 1; i <= n; i++) cin>>color[i];
+
+	for (int q = 1; q <= queries; q++) {
+        long long a, b;
+        cin>>a>>b;
+        int best = 0, secondBest = 0;
+        memset(dp, 0, sizeof(dp));
+        memset(valid, false, sizeof(valid));
+
+        for (int i = 1; i <= n; i++) {
+            int c = color[i];
+            if (valid[c])
+                dp[c] = max(dp[c], dp[c] + a*val[i]);
+            else
+                dp[c] = b*val[i];
+            if (best == c)
+                dp[c] = max(dp[c], dp[secondBest] + b*val[i]);
+            else
+                dp[c] = max(dp[c], dp[best] + b*val[i]);
     
-  }
-  return 0;
+            if (dp[c] >= dp[best]) {
+                int tmp = best;
+                best = c;
+                if (tmp != c) secondBest = tmp;
+            } else if (dp[c] >= dp[secondBest]) {
+                secondBest = c;
+            }
+            valid[c] = true;
+        }
+
+        long long ans = 0;
+        for (int i = 1; i <= n; i++) ans = max(ans, dp[i]);
+        cout<<ans<<endl;
+    }
+	return 0;
 }
